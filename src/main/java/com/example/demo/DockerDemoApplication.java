9b0030872b3e18732bbd4adeec04e2bc4edb8c21
张治中 <15171435240@139.com>
2019-12-26 14:53:27 +0800

    项目初始化

---
+++ src/main/java/com/example/demo/DockerDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+		System.out.println("启动成功");
+	}
+
+}
---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+/**
+ * @Description
+ * @auther zhangzhizhong
+ * @create 2019-12-24 15:23
+ */
+@RestController
+public class TestController {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/index")
+    public String index(ModelMap model) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        logger.info(localDateTime + ":访问");
+        // 初始化柜子
+        return "ok";
+    }
+}
+
